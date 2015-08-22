76338c0c86989479be71d56d4837a5e48aba5f15
Abe Vos <abe_vos@msn.com>
2015-08-22 15:19:12 +0200

    Hoofdmenu toegevoegd
    Het ziet er prachtig uit!

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class MainMenu : MonoBehaviour
+{
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 3, Screen.height / 3, Screen.width / 3, Screen.height / 3), "Press Space to Start.");
+    }
+}
