cffe93d4fd143f011e53111a427213b52ae45254
Mathijs Lemmers <mathijs750@gmail.com>
2015-08-23 12:48:14 +0200

    small tweak mouse rotate

--- Assets/Scripts/MouseRotate.cs
+++ Assets/Scripts/MouseRotate.cs
@@ -7,7 +7,7 @@ public class MouseRotate : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
+        Cursor.visible = false;
         angle = 0;
     }
 
@@ -16,10 +16,11 @@ public class MouseRotate : MonoBehaviour
         angle += Mathf.Clamp(Input.GetAxis("Mouse X"), -1, 1);
 
         angle = Mathf.Clamp(angle, -40, 40);
-        angle = Mathf.Lerp(angle, 0, 0.01f);
+        //angle = Mathf.Lerp(angle, 0, 0.01f);
+        angle = Mathf.Lerp(angle, 0, Time.deltaTime);
+        Debug.Log(Time.deltaTime);
 
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, angle, transform.localEulerAngles.z);
 
-        print(transform.localEulerAngles.y);
     }
 }
