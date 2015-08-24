33a108d9c8dec14cecc33a4319efc8f2cfe84a09
Abe Vos <abe_vos@msn.com>
2015-08-24 14:33:30 +0200

    Hoppa hoekberekening

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -22,7 +22,23 @@ public class Spawner : MonoBehaviour
 	
 	void Update ()
     {
+        /*
+         *  Dit is niet moeder's mooiste, oordeel niet :'(
+         */
+
         float distance = Vector3.Distance(cam.position, transform.position);
+
+        Vector3 pos3 = - Vector3.Normalize(cam.position - transform.position.normalized);
+        Vector2 pos = new Vector2(pos3.x, pos3.z).normalized;
+        Vector3 dir3 = cam.forward.normalized;
+        Vector2 dir = new Vector2(dir3.x, dir3.z).normalized;
+
+        Debug.DrawRay(Vector3.zero, dir);
+        Debug.DrawRay(Vector3.zero, pos);
+
+        float dot = Vector3.Dot(dir, pos);
+
+        print(pos + ", " + dot);
 	}
 
     void OnDrawGizmos()
