cec704a129cf8c7f074ce62cd4748de171d4b013
Abe Vos <abe_vos@msn.com>
2015-08-24 11:41:08 +0200

    Konijntjes met kleurtjes
    En bloem

---
+++ Assets/Scripts/Groeibloem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Roodkapje/Groeibloem")]
+public class Groeibloem : MonoBehaviour
+{
+    //  Poem tie doem, ik ben een groeibloem
+
+    public float groeiduur = 1.5f;
+
+    private float t = 0;
+	
+	void Update ()
+    {
+        if (t < 1)
+            t += Time.deltaTime / groeiduur;
+
+        transform.localScale = Vector3.one * Mathf.Lerp(0, 1, t);
+	}
+}
